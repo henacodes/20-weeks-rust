260e9a3600006269ce289e7a06b03ce3a62378dc
kirakos <henacodes@gmail.com>
2025-08-28 19:22:40 +0300

    day 4

--- practice/src/main.rs
+++ practice/src/main.rs
@@ -1,3 +1,20 @@
+
 fn main () {
-    
+
+    enum Santim {
+        Chichifo,
+       Simuni,
+       Amsa(String),
+   }
+
+   
+   fn value_of_cent(cent:Santim) -> u8 {
+
+    if let Santim::Simuni = cent {
+        return 25
+    } else {
+        return 1
+    }
+}
+
 }
